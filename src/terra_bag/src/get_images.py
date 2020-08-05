c40f7454bc893e16526eaa4aa5ed23dc775b3543
Justin <jbwasse2@illinois.edu>
2020-08-05 10:45:55 -0500

    refactor

--- src/terra_bag/src/get_images.py
+++ src/terra_bag/src/get_images.py
@@ -34,40 +34,35 @@ def main():
 
     bag = rosbag.Bag(args.bag_file, "r")
     bridge = CvBridge()
-    count = 0
-    count_skip = 0
     file_count = 0
-    #I just read this manually form the bag cause I'm lazy
-    skip_frequency = int(112174 / 14687)
-    for topic, msg, t in bag.read_messages(topics=[args.odom_topic]):
-        if count % skip_frequency != 0:
+    pu.db
+    flag = 0
+    count = 0
+    for topic, msg, t in bag.read_messages(topics=[args.odom_topic, args.image_topic]):
+        if topic != args.odom_topic:
+            flag = 1#Get next odom
+            cv_img = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+            cv_img = cv2.resize(cv_img, dsize=(64, 64), interpolation=cv2.INTER_CUBIC)
+
+            cv2.imwrite(os.path.join(args.output_dir, "%05i.tiff" % count), cv_img)
+            print("Wrote image %i" % count)
+
             count += 1
             continue
-        count += 1
-        position = msg.pose.pose.position
-        #orientation is quaternion
-        orientation = msg.pose.pose.orientation
-        #euler in form of RPY
-        euler_rotation = euler_from_quaternion([orientation.x, orientation.y, orientation.z, orientation.w])
-        yaw = euler_rotation[2]
-        print_string = "heading: " + str(yaw) + "\npos:\n" + "- " + str(position.x) + "\n- " + str(position.y)
-        file_name = str(file_count).zfill(5) + ".yaml"
-        file_count += 1
-        f = open(os.path.join(args.output_dir, file_name), 'w')
-        f.write(print_string)
-        f.close()
-
-        
-
-    count = 0
-    for topic, msg, t in bag.read_messages(topics=[args.image_topic]):
-        cv_img = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
-        cv_img = cv2.resize(cv_img, dsize=(64, 64), interpolation=cv2.INTER_CUBIC)
-
-        cv2.imwrite(os.path.join(args.output_dir, "%05i.tiff" % count), cv_img)
-        print("Wrote image %i" % count)
-
-        count += 1
+        if flag == 1:
+            position = msg.pose.pose.position
+            #orientation is quaternion
+            orientation = msg.pose.pose.orientation
+            #euler in form of RPY
+            euler_rotation = euler_from_quaternion([orientation.x, orientation.y, orientation.z, orientation.w])
+            yaw = euler_rotation[2]
+            print_string = "heading: " + str(yaw) + "\npos:\n" + "- " + str(position.x) + "\n- " + str(position.y)
+            file_name = str(file_count).zfill(5) + ".yaml"
+            file_count += 1
+            f = open(os.path.join(args.output_dir, file_name), 'w')
+            f.write(print_string)
+            f.close()
+            flag = 0
 
     bag.close()
 
